63f95d49894968cbcea4d77388414365b89fb46b
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2021-03-03 04:10:37 -0800

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,1 +1,42 @@
+ 
 
+import sys
+import os
+import hashlib
+
+
+
+
+
+def comphash(file):
+
+    BUF_SIZE = 65536
+
+    sha256 = hashlib.sha256()
+
+    with open(file, 'rb') as f:
+
+         while True:
+
+               data = f.read(BUF_SIZE)
+
+               if not data:
+
+                break
+
+                     sha256.update(data)
+
+    return sha256.hexdigest()
+
+
+file1 = comphash(sys.argv[1])
+
+file2 = comphash(sys.argv[2])
+
+if file1 == file2:
+
+   print("Both files are same")
+
+else:
+
+   print("Files are different!")
